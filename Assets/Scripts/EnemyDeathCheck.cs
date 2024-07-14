8e5a7218c5e8373e148fb68426d55207895ce9eb
Ryan Abusaada <71282474+gZation@users.noreply.github.com>
2024-07-14 19:29:07 -0400

    Music fun

--- Assets/Scripts/EnemyDeathCheck.cs
+++ Assets/Scripts/EnemyDeathCheck.cs
@@ -6,12 +6,17 @@ public class EnemyDeathCheck : MonoBehaviour
 {
     [SerializeField] private Transform enemies;
     [SerializeField] private Animator animator;
+    private AudioSource audioSource;
     private bool beaten;
-
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     private void Update()
     {
         if (enemies.transform.childCount > 0 || beaten) return;
         beaten = true;
         animator.Play("reveal");
+        audioSource.Play();
     }
 }
